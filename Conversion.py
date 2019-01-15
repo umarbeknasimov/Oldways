fa3584037d363708fc1d3ef2806cad59974154ec
Sophia Zhi <sophiazhi@gmail.com>
2019-01-15 16:40:38 -0500

    sku

--- Conversion.py
+++ Conversion.py
@@ -1,13 +1,14 @@
 import xlrd 
 from xlwt import Workbook
 
-# loc = ("C:/Users/Umarbek Nasimov/Desktop/Oldways/StoreOrdersExport_Example.xlsx")
-loc = ("/Users/sophiazhi/Documents/GitHub/Oldways/StoreOrdersExport_Example.xlsx")
 
-wb = xlrd.open_workbook(loc)
+loc_order_report = ("StoreOrdersExport_Example.xlsx")
+loc_sku_map = ("SKU_class_item.xlsx")
+
+wb = xlrd.open_workbook(loc_order_report)
 sheet = wb.sheet_by_index(0) #make a new sheet object
 
-'''
+
 num_of_rows=sheet.nrows #number of rows
 num_of_cols=sheet.ncols #number of columns
 print(num_of_rows, num_of_cols)
@@ -15,7 +16,7 @@ print(sheet.cell_value(1,1)) #get the specific text at row 1, col 1
 
 for i in range(num_of_cols):
 	print(sheet.cell_value(0,i)) #print all the column names 
-'''
+
 
 wb_new = Workbook()
 sheet1 = wb_new.add_sheet("Modified Workbook")
