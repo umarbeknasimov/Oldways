bba173340acc427f95c12aa988ae3af4490949dd
Sophia Zhi <sophiazhi@gmail.com>
2019-01-15 17:15:09 -0500

    change to openpyxl

--- Conversion.py
+++ Conversion.py
@@ -1,14 +1,17 @@
-import xlrd 
-from xlwt import Workbook
+# import xlrd # read xlsx or xls files
+# from xlwt import Workbook # write xls files
+from openpyxl import Workbook, load_workbook # read and modify Excel 2010 files
 
 
 loc_order_report = ("StoreOrdersExport_Example.xlsx")
 loc_sku_map = ("SKU_class_item.xlsx")
 
-wb = xlrd.open_workbook(loc_order_report)
-sheet = wb.sheet_by_index(0) #make a new sheet object
+wb_order_report = load_workbook(loc_order_report)
+# sheet = wb_order_report.sheet_by_index(0) #make a new sheet object
 
+wb_sku_map = load_workbook(loc_sku_map)
 
+'''
 num_of_rows=sheet.nrows #number of rows
 num_of_cols=sheet.ncols #number of columns
 print(num_of_rows, num_of_cols)
@@ -16,24 +19,19 @@ print(sheet.cell_value(1,1)) #get the specific text at row 1, col 1
 
 for i in range(num_of_cols):
 	print(sheet.cell_value(0,i)) #print all the column names 
-
+'''
 
 wb_new = Workbook()
-sheet1 = wb_new.add_sheet("Modified Workbook")
+# sheet1 = wb_new.add_sheet("Modified Workbook") # xlwt 
+ws1 = wb_new.active
+ws1.title = "Sales Receipts"
 
-sheet1.write(0,0, "Customer")
-sheet1.write(0,1, "Date")
-sheet1.write(0,2, "Ref No.")
-sheet1.write(0,3, "Class")
-sheet1.write(0,4, "Payment method")
-sheet1.write(0,5, "Memo")
-sheet1.write(0,6, "Item")
-sheet1.write(0,7, "Quantity")
-sheet1.write(0,8, "Amount")
-sheet1.write(0,9, "Amount of Sales Receipt")
-sheet1.write(0,10, "Amount of transaction")
-sheet1.write(0,11, "Amount Deposited")
-sheet1.write(0,12, "Date deposited to CTC")
-sheet1.write(0,13, "Template Name")
 
-wb_new.save("test.xls")
+# write column names
+column_names = ['Customer', 'Date', 'Ref No.', 'Class', 'Payment method', 'Memo', 'Item', 'Quantity', 'Amount', 'Amount of Sales Receipt', 'Amount of transaction', 'Amount Deposited', 'Date deposited to CTC', 'Template Name']
+
+for i in range(len(column_names)):
+	ws1.cell(0+1, i+1, column_names[i])
+
+
+wb_new.save("Sales Receipts.xlsx")
