226eadf279a5614bc4e356b8f622e1219616310a
Joshua Ani <josh4820@gmail.com>
2019-01-17 21:31:49 -0500

    Added example of more than 1 item

--- Conversion.py
+++ Conversion.py
@@ -18,7 +18,7 @@ print(num_of_rows, num_of_cols)
 print(sheet.cell_value(1,1)) #get the specific text at row 1, col 1
 
 for i in range(num_of_cols):
-	print(sheet.cell_value(0,i)) #print all the column names 
+    print(sheet.cell_value(0,i)) #print all the column names 
 '''
 
 wb_new = Workbook()
@@ -31,24 +31,22 @@ ws1.title = "Sales Receipts"
 column_names = ['Customer', 'Date', 'Ref No.', 'Class', 'Payment method', 'Memo', 'Item', 'Quantity', 'Amount', 'Amount of Sales Receipt', 'Amount of transaction', 'Amount Deposited', 'Date deposited to CTC', 'Template Name']
 
 for i in range(len(column_names)):
-	ws1.cell(0+1, i+1, column_names[i])
+    ws1.cell(0+1, i+1, column_names[i])
 
- # for each row in store order report
- 	# for each number in total quanitity
- 		# customer
- 		# date
- 		# ref no
- 		# search up class from sku
- 		# payment
- 		# memo
- 		#item based on sku
- 		# quantity unknown
- 		# amount of sales received unknown
- 		# amount of transaction = cost of total order
- 		# amount deposited blank
- 		# column M blank
- 		template name = Customer Sales Receipt
-
-
+# for each row in store order report
+    # for each number in total quanitity
+        # customer
+        # date
+        # ref no
+        # search up class from sku
+        # payment
+        # memo
+        # item based on sku
+        # quantity unknown
+        # amount of sales received unknown
+        # amount of transaction = cost of total order
+        # amount deposited blank
+        # column M blank
+# template name = Customer Sales Receipt
 
 wb_new.save("Sales Receipts.xlsx")
