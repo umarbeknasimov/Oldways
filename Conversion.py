8598907422a3a9f0dbb5f2a661f6c1213f3592d8
Sophia Zhi <sophiazhi@gmail.com>
2019-01-23 15:12:44 -0500

    Revert "more cells"
    This reverts commit 56f9e24982ce5aef9359ebf4d703864c1c249bae.

--- Conversion.py
+++ Conversion.py
@@ -6,17 +6,17 @@ from openpyxl import Workbook, load_workbook # read and modify Excel 2010 files
 loc_order_report = ("StoreOrdersExport_Example.xlsx")
 loc_sku_map = ("SKU_class_item.xlsx")
 
-wb_order_report = load_workbook(loc_order_report, data_only=False)
+wb_order_report = load_workbook(loc_order_report)
 order_sheet = wb_order_report.worksheets[0] #make a new sheet object
 
 wb_sku_map = load_workbook(loc_sku_map)
-sku_sheet = wb_sku_map.worksheets[0]
+
 
 num_rows=order_sheet.max_row #number of rows
 num_cols=order_sheet.max_column #number of columns
 
-# print(num_rows, num_cols)
-# print(order_sheet['A1'].value)
+print(num_rows, num_cols)
+print(order_sheet['A1'].value)
 '''
 print(sheet.cell_value(1,1)) #get the specific text at row 1, col 1
 
@@ -47,17 +47,10 @@ for row in range(2, num_rows+1):
  	customer_name = order_sheet['N'+str(row)].value
  	ws1.cell(row, 3, customer_name)
  	
- 	sku_row = 0
- 	for j in range(1,sku_sheet.max_row+1):
- 		if sku_sheet['C'+str(j)].value == order_sheet['BD'+str(row)].value:
- 			sku_row = j
 
- 	if sku_row == 0:
- 		# leave class blank because we don't know the sku
- 		ws1.cell(row, 4, "")
- 	else:
- 		sku_class = sku_sheet['A'+str(sku_row)].value
- 		ws1.cell(row, 4, sku_class)
+ 	# search up class from sku
+ 		# SKU in column BD
+ 		# umarbek will personally search it up in the other excdel sheet
  	
  	payment_method = order_sheet['R'+str(row)].value
  	ws1.cell(row, 5, payment_method)
@@ -65,26 +58,17 @@ for row in range(2, num_rows+1):
  	memo = order_sheet['A'+str(row)].value
  	ws1.cell(row, 6, memo)
 
- 	if sku_row == 0:
- 		# leave item blank because we don't know the sku
- 		ws1.cell(row, 7, "")
- 	else:
- 		sku_item = sku_sheet['B'+str(sku_row)].value
- 		ws1.cell(row, 7, sku_item)
+ 	#item based on sku
  	
  	# quantity unknown
  	
  	# amount of sales received unknown
  	
  	# amount of transaction = cost of total order
- 	transaction = order_sheet['K'+str(row)].value
- 	ws1.cell(row, 11, transaction)
  	
  	# amount deposited blank
- 	ws1.cell(row, 12, "")
  	
  	# column M blank
- 	ws1.cell(row, 13, "")
  	
  	ws1.cell(row, 14, "Custom Sales Receipt")
 
